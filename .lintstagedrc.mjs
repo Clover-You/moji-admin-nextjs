edf6d49c359b773e5d73d3feb5669468f15960e0
Clover <2621869236@qq.com>
2024-06-03 12:33:04 +0800

    chore: use husky and lint-staged to repeace pre-commit

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,13 @@
+import path from "node:path"
+import process from "node:process"
+
+const buildEslintCommand = createCommand("eslint --fix ", "")
+
+function createCommand(prefix, join) {
+  return (filenames) =>
+    `${prefix} ${filenames.map((f) => path.relative(process.cwd(), f)).join(` ${join} `)}`
+}
+
+export default {
+  "*.{js,jsx,ts,tsx,vue}": [buildEslintCommand],
+}
